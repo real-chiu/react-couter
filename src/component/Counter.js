2dac1eb652c56c2e5eee01409bb3100b08014f76
real-chiu <real.chiu@oocl.com>
2020-04-21 15:05:20 +0800

    finished counter

---
+++ src/component/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+ class Counter extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.onIncrementCounter = this.onIncrementCounter.bind(this);
+    this.onDecrementCounter = this.onDecrementCounter.bind(this);
+    this.state = {
+       count: 0
+    }
+  }
+
+  onIncrementCounter = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    });
+  }
+
+  onDecrementCounter = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      }
+    });
+  }
+  
+  
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrementCounter}>+</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.onDecrementCounter}>-</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
