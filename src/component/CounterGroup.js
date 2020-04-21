8c487c9afcc3f29c7093a886160e123c49d6925d
real-chiu <real.chiu@oocl.com>
2020-04-21 16:14:47 +0800

    generate dynamic number of counters

--- src/component/CounterGroup.js
+++ src/component/CounterGroup.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react'
 import Counter from './Counter';
 
 export default class CounterGroup extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      size: 3,
+    }
+  }
+  
+  handleValueChange = (event) => {
+    const value = event.target.value
+    this.setState({
+        size: value.length > 0 ? parseInt(value) : 0,
+    });
+  }
 
   initArray = (size) => {
     return Array.from(Array(size).keys());
   }
 
   render() {
-    let counters = this.initArray(100);
+    let counters = this.initArray(this.state.size);
     return (
       <div>
+        <form>
+          <input type="text"  value={this.state.size} onChange={this.handleValueChange}/>
+        </form>
         {
           counters.map((value) =><Counter key={value}/>)
         }
