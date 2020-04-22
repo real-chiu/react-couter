b712e09d7b36e942375b22eb31a4673282f9a363
real-chiu <real.chiu@oocl.com>
2020-04-22 16:59:33 +0800

    click text to delete/undelete todoItem

--- src/components/TodoElement.js
+++ src/components/TodoElement.js
@@ -10,17 +10,27 @@ export default class TodoElement extends Component {
   constructor(props) {
     super(props)
 
+    this.changeStatus = this.changeStatus.bind(this);
+
     this.state = {
-      status: props.status
+      status: props.info.status
     }
+  }
+
+  changeStatus = () => {
+    this.setState((prevState) => {
+      return {
+        status: !prevState.status
+      }
+    })
   }
 
   render() {
     return (
       <Card style={cardStyle} hoverable size="small">
         <Row justify="space-between">
-          <Col>
-            <Text>
+          <Col  onClick={this.changeStatus}>
+            <Text delete={this.state.status}>
               {this.props.info.content}
             </Text>
           </Col>
