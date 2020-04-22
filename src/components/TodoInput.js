125d87097ae4d0ea902f311053d4b7662dde0fe7
real-chiu <real.chiu@oocl.com>
2020-04-22 16:31:58 +0800

    get user input

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react'
 import { Input } from 'antd';
 
 export default class TodoInput extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      inputValue: ""
+    }
+  }
+  
+  onChange = (event) => {
+    console.log("User input:" + event.target.value);
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
   render() {
     return (
-      <Input placeholder="Enter todo item" size="large"/>
+      <Input 
+        placeholder="Enter todo item" 
+        size="large"
+        value={this.state.inputValue}
+        onChange={this.onChange}
+      />
     )
   }
 }
